1888d2348281adfc7789c277deabc463bccc1d65
ligx <ligongxing@ricebook.com>
2016-08-02 22:29:44 +0800

    update kafka logic

--- src/main/scala/com/ligx/AkkaKafka/Main.scala
+++ src/main/scala/com/ligx/AkkaKafka/Main.scala
@@ -10,8 +10,9 @@ object Main extends App{
   val system = ActorSystem("AkkaSystem")
 
   val topicConfigs = Seq(
-    TopicConfig(topic = "akka-topic", numConsumerThread = 1),
-    TopicConfig(topic = "akka-topic2", numConsumerThread = 1)
+//    TopicConfig(topic = "akka-topic", numConsumerThread = 1),
+//    TopicConfig(topic = "akka-topic2", numConsumerThread = 1)
+    TopicConfig(topic = "order", numConsumerThread = 1)
   )
 
   val kafkaActor = KafkaActor(system, topicConfigs)
