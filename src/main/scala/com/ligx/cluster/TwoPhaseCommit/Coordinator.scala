b8c30282ec2538c904e888cecc0913af46d25c50
ligx <ligongxing@ricebook.com>
2016-08-16 17:50:25 +0800

    2pc implement with akka

--- src/main/scala/com/ligx/cluster/TwoPhaseCommit/Coordinator.scala
+++ src/main/scala/com/ligx/cluster/TwoPhaseCommit/Coordinator.scala
@@ -1,7 +1,8 @@
 package com.ligx.cluster.TwoPhaseCommit
 
-import akka.actor.{Actor, Props}
-import com.ligx.cluster.TwoPhaseCommit.example.Process
+import akka.actor.{Actor, PoisonPill, Props}
+import akka.pattern.ask
+import com.ligx.cluster.TwoPhaseCommit.Helper._
 
 import scala.reflect.ClassTag
 
@@ -9,37 +10,48 @@ import scala.reflect.ClassTag
   * Created by ligx on 16/8/15.
   */
 abstract class Coordinator[T, TA: ClassTag] extends Actor {
+
   import context.dispatcher
-  private def transactor(id: String) = context.child(id) getOrElse context.actorOf(Props(classTag[TA].runtimeClass), id)
+
+  private def transactor(id: String) = context.child(id) getOrElse context.actorOf(Props(implicitly[ClassTag[TA]].runtimeClass), id)
+
   final def receive = {
-    case rs: ReqSeq[T] => import rs.askTimeout; transactor(rs.tid) ? Process(rs) pipeTo sender
+    case rs: ReqSeq[T] => import rs.askTimeout; transactor(rs.tid) ? Process(rs) foreach (sender ! _)
   }
 }
 
-abstract class Transactor[T, P <: Processor[T]: ClassTag] extends TransactorLike[T] {
-  def process(id: String) = context.child(id) getOrElse context.actorOf(Props(classTag[P].runtimeClass), id)
+abstract class Transactor[T, P <: Processor[T] : ClassTag] extends TransactorLike[T] {
+  def process(id: String) = context.child(id) getOrElse context.actorOf(Props(implicitly[ClassTag[P]].runtimeClass), id)
+
   final def receive = {
     case p: Process[T] =>
       scheduleTimeouts(parent == null, p.rs.merging(acc, votes))
-      transact = p.rs; parent = sender
-      for ((r, i) <- p.rs.data.zipWithIndex) process(p.rs.tid) ! Req(tid, r, acc.size + i)
+      transact = p.rs
+      parent = sender
+      for ((r, i) <- p.rs.data.zipWithIndex) process(p.rs.tid) ! Req(tid, r, acc.size + i)   // 1 向参与者发送事务请求
       acc ++= p.rs.data
     case v: Vote[T] =>
       stat += sender -> v
       for (newVote <- transact.merging(acc, votes).apply; (actor, vote) <- stat) actor ! newVote(vote.req)
     case a: Ack[T] =>
       stat -= stat.find(_._2.req == a.vote.req).get
-      if (stat.isEmpty) { a.vote.isCommit toOption commit getOrElse rollback; self ! PoisonPill }
+      if (stat.isEmpty) {
+        a.vote.isCommit toOption commit getOrElse rollback; self ! PoisonPill
+      }
     case "Timeout" =>
-      if (stat.nonEmpty) { for ((actor, vote) <- stat) actor ! Rollback(vote.req); rollback }; self ! PoisonPill
+      if (stat.nonEmpty) {
+        for ((actor, vote) <- stat) actor ! Rollback(vote.req); rollback
+      }; self ! PoisonPill
   }
 }
 
 trait Processor[T] extends ProcessorLike[T] {
+
   import context.dispatcher
+
   final def receive = {
-    case r: Req[T] => process(r) pipeTo sender
-    case o: Commit[T] => complete(o.req) map (_ => Ack(o)) pipeTo sender
-    case o: Rollback[T] => rollback(o.req) map (_ => Ack(o)) pipeTo sender
+    case r: Req[T] => process(r) foreach (sender ! _)    // 执行事务,向协调者发送vote yes/no
+    case o: Commit[T] => complete(o.req) map (_ => Ack(o)) foreach (sender ! _)
+    case o: Rollback[T] => rollback(o.req) map (_ => Ack(o)) foreach (sender ! _)
   }
 }
--- src/main/scala/com/ligx/cluster/TwoPhaseCommit/package.scala
+++ src/main/scala/com/ligx/cluster/TwoPhaseCommit/package.scala
@@ -1,16 +1,16 @@
-package com.ligx.cluster.TwoPhaseCommit
+package com.ligx.cluster
 
 import akka.actor.{Actor, ActorRef}
 import akka.util.Timeout
 
 import scala.collection.mutable.ListBuffer
-import scala.concurrent.Future
 import scala.concurrent.duration._
+import scala.concurrent.Future
 
 /**
-  * Created by ligx on 16/8/15.
+  * Created by ligx on 16/8/16.
   */
-package object example {
+package object TwoPhaseCommit {
 
   object Helper {
 
@@ -24,9 +24,10 @@ package object example {
 
     case class expecting[T](override val transactionSize: Int)(val acc: Seq[T], val votes: Seq[Vote[T]]) extends Merging[T]
 
-    case class default[T] private[example](acc: Seq[T], votes: Seq[Vote[T]]) extends Merging[T]
+    case class default[T] private[example] (acc: Seq[T], votes: Seq[Vote[T]]) extends Merging[T]
 
     type GetMerge[T] = (Seq[T], Seq[Vote[T]]) => Merging[T]
+
     val defaultTimeout = 5 seconds
 
     object implic {
@@ -61,42 +62,40 @@ package object example {
   }
 
 
-  case class ReqSeq[T](tid: String, data: Seq[T])
-                      (implicit val merging: GetMerge[T] = default[T] _, val askTimeout: Timeout = Timeout(defaultTimeout))
-
-  case class Req[T](tid: String, body: T, seqNumber: Int)
 
   case class Process[T](rs: ReqSeq[T])
+  case class ReqSeq[T](tid: String, data: Seq[T])
+                      (implicit val merging: GetMerge[T] = default[T] _, val askTimeout: Timeout = Timeout(defaultTimeout))
+  case class Req[T](tid: String, body: T, seqNumber: Int)
+
+
+
 
   trait Result
-
   object Success extends Result
-
   object Failure extends Result
+
 
   trait Vote[T] {
     def req: Req[T];
-
     def isCommit: Boolean
   }
-
   case class Commit[T](req: Req[T]) extends Vote[T] {
     def isCommit = true
   }
-
   case class Rollback[T](req: Req[T]) extends Vote[T] {
     def isCommit = false
   }
 
   case class Ack[T](vote: Vote[T])
 
-  abstract class TransactorLike[T] extends Actor {
 
+
+  abstract class TransactorLike[T] extends Actor {
     import context.dispatcher
 
     private[example] var transact: ReqSeq[T] = _
-    private[example] var parent: ActorRef = _ //we need it to correlate with last request, it's not context.parent
-
+    private[example] var parent: ActorRef = _     //we need it to correlate with last request, it's not context.parent
     private[example] val acc = ListBuffer[T]()
 
     def commit = parent ! Success
@@ -111,10 +110,13 @@ package object example {
     private[example] def votes = stat.map(_._2).toSeq // voting
 
     def scheduleTimeouts(isFirstChunk: Boolean, merging: Merging[T]) = {
+      // 5 seconds之后向自身发送Timeout消息
       if (isFirstChunk) context.system.scheduler.scheduleOnce(merging.transactionTimeout, self, "Timeout")
       merging.chunkTimeout foreach (context.system.scheduler.scheduleOnce(_, self, "Timeout"))
     }
   }
+
+
 
   trait ProcessorLike[T] extends Actor {
     def process(r: Req[T]): Future[Vote[T]]
@@ -123,5 +125,4 @@ package object example {
 
     def rollback(t: Req[T]): Future[Unit]
   }
-
 }
